025fc191bc552a1dc4ef0b536e21ad458c34f0ce
Rohit <rohits8731@gmail.com>
2026-09-08 17:03:21 +0530

    06: Events Page

--- app/(main)/events/page.tsx
+++ app/(main)/events/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { auth } from "@clerk/nextjs/server";
+import { CalendarPlus } from "lucide-react";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
 
@@ -9,7 +12,21 @@ const EventsPage = async () => {
         redirect("/login");
     }
 
-    return <div>EventsPage</div>;
+    return (
+        <section className="flex flex-col items-center gap-16 animate-fade-in">
+            <div className="flex gap-4 items-baseline">
+                <h1 className="text-4xl xl:text-5xl font-black mb-6">Events</h1>
+                <Button
+                    className="bg-blue-500 hover:bg-blue-400 text-white py-6 hover:scale-110 duration-500 border-b-4 border-blue-700 hover:border-blue-500 rounded-2xl shadow-accent-foreground text-2xl font-black"
+                    asChild
+                >
+                    <Link href="/events/new">
+                        <CalendarPlus className="mr-4 size-7" /> Create Event
+                    </Link>
+                </Button>
+            </div>
+        </section>
+    );
 };
 
 export default EventsPage;
