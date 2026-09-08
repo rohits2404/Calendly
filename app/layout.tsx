8031c6c7bbd443fcf788cf7f3670bcc1d9e83bd9
Rohit <rohits8731@gmail.com>
2026-09-08 15:59:41 +0530

    01: Project Setup

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Chrona",
+    description:
+        "Chrona Is A Simple And Efficient Calendar App That Helps You Manage Your Events, Meetings, And Schedules With Ease. Stay Organized And Never Miss An Important Date Again!",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased animate-fade-in`}
+            >
+                {children}
+                <Toaster />
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="font-bold text-5xl text-center">Chrona App</h1>
+        </div>
+    );
+};
+
+export default Home;
