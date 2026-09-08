5f500aac8cc18bc0379d2ea39ea2f44106eef604
Rohit <rohits8731@gmail.com>
2026-09-08 23:29:20 +0530

    12: Schedule Feature

---
+++ app/(main)/schedule/page.tsx
@@ -0,0 +1,25 @@
+import { getSchedule } from "@/actions/schedule";
+import { ScheduleForm } from "@/components/forms/ScheduleForm";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { auth } from "@clerk/nextjs/server";
+import React from "react";
+
+const SchedulePage = async () => {
+    const { userId, redirectToSignIn } = await auth();
+    if (!userId) return redirectToSignIn();
+
+    const schedule = await getSchedule(userId);
+
+    return (
+        <Card className="max-w-md mx-auto border-8 border-blue-200 shadow-2xl shadow-accent-foreground">
+            <CardHeader>
+                <CardTitle>Schedule</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <ScheduleForm schedule={schedule ?? undefined} />
+            </CardContent>
+        </Card>
+    );
+};
+
+export default SchedulePage;
