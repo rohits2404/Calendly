2c1275ce8c3bebd9e9b78a3394b696a31b750583
Rohit <rohits8731@gmail.com>
2026-09-09 01:00:37 +0530

    15: Booking Events Page

--- server/actions/schedule.ts
+++ server/actions/schedule.ts
@@ -8,6 +8,23 @@ import { eq } from "drizzle-orm";
 import { BatchItem } from "drizzle-orm/batch";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
+import { getCalendarEventTimes } from "../google/googleCalendar";
+import {
+    addMinutes,
+    areIntervalsOverlapping,
+    isFriday,
+    isMonday,
+    isSaturday,
+    isSunday,
+    isThursday,
+    isTuesday,
+    isWednesday,
+    isWithinInterval,
+    setHours,
+    setMinutes,
+} from "date-fns";
+import { DAYS_OF_WEEK_IN_ORDER } from "@/constants";
+import { fromZonedTime } from "date-fns-tz";
 
 type ScheduleRow = typeof ScheduleTable.$inferSelect;
 type AvailabilityRow = typeof ScheduleAvailabilityTable.$inferSelect;
@@ -87,4 +104,100 @@ export async function getSchedule(
         ...schedule[0],
         availabilities,
     };
+}
+
+function getAvailabilities(
+    groupedAvailabilities: Partial<
+        Record<
+            (typeof DAYS_OF_WEEK_IN_ORDER)[number],
+            (typeof ScheduleAvailabilityTable.$inferSelect)[]
+        >
+    >,
+    date: Date,
+    timezone: string,
+): { start: Date; end: Date }[] {
+    const dayOfWeek = (() => {
+        if (isMonday(date)) return "monday";
+        if (isTuesday(date)) return "tuesday";
+        if (isWednesday(date)) return "wednesday";
+        if (isThursday(date)) return "thursday";
+        if (isFriday(date)) return "friday";
+        if (isSaturday(date)) return "saturday";
+        if (isSunday(date)) return "sunday";
+        return null;
+    })();
+
+    if (!dayOfWeek) return [];
+
+    const dayAvailabilities = groupedAvailabilities[dayOfWeek];
+
+    if (!dayAvailabilities) return [];
+
+    return dayAvailabilities.map(({ startTime, endTime }) => {
+        const [startHour, startMinute] = startTime.split(":").map(Number);
+        const [endHour, endMinute] = endTime.split(":").map(Number);
+
+        const start = fromZonedTime(
+            setMinutes(setHours(date, startHour), startMinute),
+            timezone,
+        );
+
+        const end = fromZonedTime(
+            setMinutes(setHours(date, endHour), endMinute),
+            timezone,
+        );
+
+        return { start, end };
+    });
+}
+
+export async function getValidTimesFromSchedule(
+    timesInOrder: Date[],
+    event: { clerkUserId: string; durationInMinutes: number },
+): Promise<Date[]> {
+    const { clerkUserId: userId, durationInMinutes } = event;
+
+    const start = timesInOrder[0];
+    const end = timesInOrder.at(-1);
+
+    if (!start || !end) return [];
+
+    const schedule = await getSchedule(userId);
+
+    if (schedule == null) return [];
+
+    const groupedAvailabilities = Object.groupBy(
+        schedule.availabilities,
+        (a) => a.dayOfWeek,
+    );
+
+    const eventTimes = await getCalendarEventTimes(userId, {
+        start,
+        end,
+    });
+
+    return timesInOrder.filter((intervalDate) => {
+        const availabilities = getAvailabilities(
+            groupedAvailabilities,
+            intervalDate,
+            schedule.timezone,
+        );
+
+        const eventInterval = {
+            start: intervalDate,
+            end: addMinutes(intervalDate, durationInMinutes),
+        };
+
+        return (
+            eventTimes.every((eventTime) => {
+                return !areIntervalsOverlapping(eventTime, eventInterval);
+            }) &&
+            availabilities.some((availability) => {
+                return (
+                    isWithinInterval(eventInterval.start, availability) &&
+                    isWithinInterval(eventInterval.end, availability)
+                );
+            })
+        );
+    });
 }
